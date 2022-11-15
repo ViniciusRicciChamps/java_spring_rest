af082d6f50093f5bde3ba42ee4c2bbfa83291722
ViniciusRicciChamps <vcricci26@hotmail.com>
2022-11-15 16:14:34 -0300

    inclusão do controller de POST

--- src/main/java/com/rest/java_spring_rest/Controllers/indexController.java
+++ src/main/java/com/rest/java_spring_rest/Controllers/indexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,45 +26,22 @@ public class indexController {
     private UsuarioRepository usuarioRepository;
 
     @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id){
-        Optional<Usuario> usuario =  usuarioRepository.findById(id);
+    public ResponseEntity<Usuario> buscarUsuarioId(@PathVariable(value = "id") Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
         return new ResponseEntity(usuario.get(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity<List<Usuario>> usuario(){
+    public ResponseEntity<List<Usuario>> buscarUsuario() {
         List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
         return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
     }
 
-    /* ************ Passando parametro na requisição
-    @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init(@RequestParam(value = "nome", defaultValue = "null") String nome){
-        System.out.println(nome);
-        return new ResponseEntity("seu nome é " + nome, HttpStatus.OK);
+    @PostMapping(value = "/cadastrar", produces = "application/json")
+    public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+
+        Usuario usuarioCadastro = usuarioRepository.save(usuario);
+        return new ResponseEntity<Usuario>(usuarioCadastro, HttpStatus.OK);
+
     }
-    
-    @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity<Usuario> iEntity(){
-
-        /*inserindo dados manualmente
-        Usuario usuario = new Usuario();
-        usuario.setId(152L);
-        usuario.setLogin("vini@ricci");
-        usuario.setSenha("12345");
-        usuario.setNome("Vinicius Costa Ricci");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(15L);
-        usuario2.setLogin("davi@ricci");
-        usuario2.setSenha("87896");
-        usuario2.setNome("Davi Ricci");
-
-        List<Usuario> usuarios = new ArrayList<Usuario>();
-        usuarios.add(usuario2);
-        usuarios.add(usuario);
-        
-        return new ResponseEntity(usuarios, HttpStatus.OK);
-        
-    }*/
 }
